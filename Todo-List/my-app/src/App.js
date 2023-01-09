c1b1469ea0568cced95ddcfde2d86585738b055b
balja0803 <balja0803@gmail.com>
2023-01-09 12:27:46 +0800

    school

---
+++ Todo-List/my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputVal, setinputVal] = useState();
+  const getInput = () => {
+    console.log("test");
+    setTodos([...todos, inputVal]);
+  };
+  const getValue = (event) => {
+    setinputVal(event.target.value);
+  };
+  return (
+    <div className="App">
+      <input type="text" onChange={getValue}></input>
+      <button onClick={getInput}>add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
